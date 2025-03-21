5b8f21574d612395354df32c841f64f3801c3539
Ricardo Coll <rickycoll2005@gmail.com>
2025-03-21 01:54:03 +0100

    Working 2 way chat. No multithreading

--- client.py
+++ client.py
@@ -1,14 +1,30 @@
 import socket
 
+host_ip = socket.gethostbyname(socket.gethostname())
+port = 2222
+bytesize = 1024
+
+
 #Creating Client socket
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #We use IPV4 and TCP
 
 #Connecting the Client Socket with an IP adress and the Server's Port
-client_socket.connect((socket.gethostbyname(socket.gethostname()), 22222))
+client_socket.connect((host_ip, port))
 
-#Recieve Messages
-message = client_socket.recv(1024)
-print(message.decode())#Make sure the String is encoded)
+#Send and Recieve Messages
+while True:
+    message = client_socket.recv(bytesize).decode() #Recieve the message and decode
+
+    #Check if server quits
+    if message == "quit":
+        client_socket.send("quit".encode()) #Send Comfirmation to the Server
+        print("\nDisconnected.")
+        break
+    #If not send message
+    else:
+        print(f"\n{message}")
+        message = input("\nClient: ")
+        client_socket.send(message.encode()) #Encode and send the message
 
 #Close
 client_socket.close()
--- server.py
+++ server.py
@@ -1,22 +1,36 @@
 import socket
 
+#Declarations
+host_ip = socket.gethostbyname(socket.gethostname())
+port = 2222
+bytesize = 1024
+
 #Creating Server socket
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #We use IPV4 and TCP
-
 #Binding the Server Socket to an IP adress and a Port
-server_socket.bind((socket.gethostbyname(socket.gethostname()), 22222))
-
+server_socket.bind((host_ip, port))
 #Listening for connections
 server_socket.listen()
+#Accept connections and get the Client's socket and adrdess
+client_socket, client_address = server_socket.accept()
+#Send notification to the client that he has been connected
+client_socket.send("Connected to Server.".encode()) #Make sure the String is encode
+print("Client Connected")
 
-
+#Send and Receive messages
 while True:
-    #Accept connections and get the Client's socket and adrdess
-    client_socket, client_address = server_socket.accept()
+    message = client_socket.recv(bytesize).decode() #Recieve Messages and decode
 
-    #Send notification to the client that he has been connected
-    client_socket.send("Connected".encode()) #Make sure the String is encoded
+    #Check if client quits
+    if message == "quit":
+        client_socket.send("quit".encode()) #Send Comfirmation to the Server
+        print("\nDisconnected.")
+        break
+    #If not send message
+    else:
+        print(f"\n{message}")
+        message = input("\nServer: ")
+        client_socket.send(message.encode())#Encode and send the message
 
-    #Close
-    server_socket.close()
-    break
+#Close
+server_socket.close()
